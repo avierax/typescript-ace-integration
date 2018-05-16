9ecb85ab34cfbcbeff5635d9d3a7db87a8becd7e
Ariel Viera <ariel.viera@gmail.com>
2018-05-16 18:23:11 -0400

    select userscript tab by default

--- tabs.js
+++ tabs.js
@@ -1,9 +1,9 @@
-function showTab(event, divid){
+function showTab(target, divid){
     for(let tab of document.querySelectorAll(".tabcontent")) {
         tab.style.display = (tab.id == divid)? "block" : "none";
     }
     for(let button of document.querySelectorAll(".tablinks")) {
-        if(button==event.currentTarget)
+        if(button==target)
             button.classList.add("active");
         else
             button.classList.remove("active");
