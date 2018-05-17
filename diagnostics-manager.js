f382fd600a2a8c62a527916492028e6dc2fe697f
Ariel Viera <ariel.viera@gmail.com>
2018-05-16 20:24:00 -0400

    include warnings and errors in diagnostics

--- diagnostics-manager.js
+++ diagnostics-manager.js
@@ -1,6 +1,9 @@
 (function () {
     var AceRange = ace.require("ace/range").Range;
     const consoleElement = document.getElementById("console");
+    const warning_include = document.querySelector("#warning-and-errors-link").import;
+    const warning_template = warning_include.querySelector(".warning");
+    const error_template = warning_include.querySelector(".error");
     window.diagnosticsManager = {
         errorMarkersIds: [],
         addDiagnostic: function (d) {
@@ -8,6 +11,13 @@
             let startPosition = d.file.getLineAndCharacterOfPosition(d.start);
             let range = new AceRange(startPosition.line, startPosition.character, startPosition.line, startPosition.character + d.length);
             this.errorMarkersIds.push(window.aceEditor.session.addMarker(range, "ace_underline_error", "text", true));
+            var errorElement = null;
+            if(d.category==1)
+                errorElement = error_template.cloneNode(true);
+            else
+                errorElement = warning_template.cloneNode(true);
+            errorElement.querySelector("span").innerText = d.messageText;
+            consoleElement.appendChild(errorElement);
         },
         clearDiagnostics: function () {
             this.errorMarkersIds.forEach(function (id) {
